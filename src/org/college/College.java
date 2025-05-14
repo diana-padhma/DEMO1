f600c2acc507cde01c52bf75250381891399dc89
diana-padhma <padhmadiana@gmail.com>
2025-05-14 18:18:47 +0530

    message

---
+++ src/org/college/College.java
@@ -0,0 +1,30 @@
+package org.college;
+
+public class College {
+	public void collegeName() {
+		System.out.println("College Name is : Tagore College of Art And Sciences");
+	}
+	public void collegecode() {
+		System.out.println("Collage Code is : 22920CR34");
+}
+	public void collegeRank() {
+		System.out.println("Collage Rank : 1st Rank");
+	}
+	public static void main (String[] args) {
+		College c = new College();
+		Student s = new Student();
+		Hostel h = new Hostel();
+		Dept d = new Dept();
+		
+		c.collegeName();
+		c.collegecode();
+		c.collegeRank();
+		s.studentName();
+		s.studentDept();
+		s.studentId();
+		h.hostelName();
+		d.deptName();
+	}
+	
+
+}
---
+++ src/org/college/Student.java
@@ -0,0 +1,14 @@
+package org.college;
+
+public class Student {
+	public void studentName() {
+		System.out.println("Student Name is : Diana");
+	}
+	public void studentDept() {
+		System.out.println("Student Department is : Computer Sciences");
+}
+	public void studentId() {
+		System.out.println("Student Id : 22191022");
+	}
+
+}
